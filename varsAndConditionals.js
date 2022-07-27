ff78d4f20b7f8fd13e44e69d387a122fdceae42b
Walter Taylor <walterdctaylor@yahoo.com>
2022-07-27 14:35:24 -0600

    firstCommit

---
+++ varsAndConditionals.js
@@ -0,0 +1,33 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+let jonsnowAttack = 25
+let jamieLannisterAttack = 35
+
+if(jonsnowAttack > jamieLannisterAttack
+    console.log('Jon has better attack')
+
+
+
+    if (jonsnowAttack > jamieLannisterAttack) 
+
+    let jonsnowHealth = 100 
+    let jonSnowDefense = 0
+
+    if (jonsnowHealth <= jamieLannisterHealth) {
+        console.log('Jon has been slain)
+    {else}
+
+    
+    
+    
+        
+    
+         
+    
+        
+        
+    
